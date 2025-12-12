5ea6a7f0e75be73b1c06be1e852b10f74112c578
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2025-12-13 01:19:12 +0500

    Task2

---
+++ Tyuiu.StarikovaVYA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.StarikovaVYA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.StarikovaVYA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Роман";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Варвара", res);
+        }
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.StarikovaVYA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.StarikovaVYA.Sprint0.Task0.V_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Варвара"));
+            Console.ReadKey();
+        }
+    }
+}
