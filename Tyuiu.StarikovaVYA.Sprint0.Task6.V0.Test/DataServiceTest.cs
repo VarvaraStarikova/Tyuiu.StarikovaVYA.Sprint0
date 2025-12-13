2f19c3ef701a76a959ce5b0d90a626ff64dc2d3f
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2025-12-13 06:52:06 +0500

    Task6

---
+++ Tyuiu.StarikovaVYA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.StarikovaVYA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        public static int[] AdditionArrays(int[] numbOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numbOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
