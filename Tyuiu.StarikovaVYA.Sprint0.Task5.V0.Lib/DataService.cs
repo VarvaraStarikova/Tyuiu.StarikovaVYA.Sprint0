726ae3efecb84876638f529b687c3b090a9c9533
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2025-12-13 06:00:42 +0500

    Task5

---
+++ Tyuiu.StarikovaVYA.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.StarikovaVYA.Sprint0.Task5.V0.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.StarikovaVYA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
